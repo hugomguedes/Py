e9810c75bd2e0e2537089fbdca362da0aefd4e7a
Hugo <hugomonteiroguedes@gmail.com>
2026-03-12 18:29:34 -0300

    conv tipos e remove duplicates add

--- pandas/conv_tipo.py
+++ pandas/conv_tipo.py
@@ -6,6 +6,7 @@ print(df["qtdePontos"].astype(int)) #convertendo para int
 # df["DtCriacao"] = df["DtCriacao"].replace({"0000-00-00 00:00:00.000": "2024-02-02 09:00:00.000"})  #substitui a data antiga pelo novo valor
 
 replace = {"0000-00-00 00:00:00.000": "2024-02-02 09:00:00.000"}
+
 df["DtCriacao"] = pd.to_datetime(df["DtCriacao"].replace(replace))
 
 print(df["DtCriacao"])
